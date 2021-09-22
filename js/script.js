dd5ba0540c244f7963f35f66fe222454f7575292
Arseniy <88400039+Astashevich@users.noreply.github.com>
2021-09-22 22:38:10 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
         
         // if (error === 0) {
         //     form.classList.add('_sending');
-        //     let responce = await fetch('form', {
+        //     let responce = await fetch('sendmail.php', {
         //         method: 'POST',
         //         body: formData
         //     });
